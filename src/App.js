dddee9401ec9a60108d29c399e63dcfb7975361b
guilhermealamo <guilhermealamo@hotmail.com>
2020-11-23 15:21:29 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import api from './services/api';
 import Header from './components/Header';
 
 
-
-
 const App = () => {
-    const [projects, setProjects] = useState(["Alcance", "Ativamente", "Frisbee", "Planeta Saber", "Calesita"])
-    const handleAddProject = () => { setProjects([...projects, `Novo Projeto ${Date.now()}`]); }
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setProjects(response.data);
+        })
+    }, [])
+
+    async function handleAddProject () {
+        // setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+        const response = await api.post('repositories', {
+            title: `Novo Lindao: ${Date.now()}`,
+            url: "https://api.github.com/users/Diego3g",
+            techs: ["teste1", "teste2", "teste3"]
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
 
     return (
         <>
@@ -16,7 +33,7 @@ const App = () => {
                 <fieldset id="menu">
                     <legend><h3>Projetos</h3></legend>
                     <ul>
-                        {projects.map(project => <li key={project}>{project}</li>)}
+                        {projects.map(project => <li key={project.id}>{project.title}</li>)}
                     </ul>
                     <button type="button" onClick={handleAddProject}>Adicionar</button>
                 </fieldset>
